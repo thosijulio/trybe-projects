f6c2136956b1547f1b41c4f6ae776f3c6f1e293a
thosijulio <thosijulio@gmail.com>
2021-03-11 23:19:38 -0300

    resolucao exercicio 12 e 14

--- 1.INTRODUCAO/5.6-TODO-LIST/script.js
+++ 1.INTRODUCAO/5.6-TODO-LIST/script.js
@@ -1,3 +1,7 @@
+window.onload = function onloadPageFunction() {
+  recoveryTasks();
+}
+
 document.getElementById('criar-tarefa').addEventListener('click', function createTask() {
   const tarefa = document.createElement('li')
   const listaTarefa = document.getElementById('lista-tarefas');
@@ -36,3 +40,27 @@ document.getElementById('remover-finalizados').addEventListener('click', functio
     document.getElementById('lista-tarefas').removeChild(document.getElementsByClassName('completed')[0]);
   }
 })
+
+document.getElementById('remover-selecionado').addEventListener('click', function deleteSelectedTasks() {
+    document.getElementById('lista-tarefas').removeChild(document.getElementsByClassName('selected')[0]);
+})
+
+document.getElementById('salvar-tarefas').addEventListener('click', function saveTasks() {
+  localStorage.clear();
+  let tarefasSalvas = {};
+    for(let index = 0; index < document.getElementsByClassName('tasks').length; index += 1) {
+      tarefasSalvas.texto = document.getElementsByClassName('tasks')[index].innerText;
+      tarefasSalvas.classes = document.getElementsByClassName('tasks')[index].className;
+      localStorage.setItem(index,JSON.stringify(tarefasSalvas));
+    }
+})
+
+function recoveryTasks() {
+  for( let index = 0; index < localStorage.length; index += 1) {
+    let tarefasRecuperadas = JSON.parse(localStorage[index]);
+    let tarefa = document.createElement('li');
+    tarefa.innerText = tarefasRecuperadas.texto;
+    tarefa.className = tarefasRecuperadas.classes;
+    document.getElementById('lista-tarefas').appendChild(tarefa);
+  }
+}
