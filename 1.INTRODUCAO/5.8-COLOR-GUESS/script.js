02a8fb998596f589fa8ba31c87d393ece6c4936a
thosijulio <thosijulio@gmail.com>
2021-03-13 15:47:18 -0300

    adicionando classe answer para o circulo correro e adicionando o seu rgb na p com id rgb-color

--- 1.INTRODUCAO/5.8-COLOR-GUESS/script.js
+++ 1.INTRODUCAO/5.8-COLOR-GUESS/script.js
@@ -1,10 +1,16 @@
 window.onload = function() {
-  colorGenerator();
+  colorGeneratorAndToGuess();
 }
 
-function colorGenerator() {
+function colorGeneratorAndToGuess() {
   let circulos = document.getElementsByClassName('ball');
+  circulos[Math.floor(Math.random() * 6)].id = 'answer';
   for(index = 0; index < circulos.length; index += 1) {
     circulos[index].style.backgroundColor = 'RGB(' + (Math.floor(Math.random() * 255) + 1) + ', ' + (Math.floor(Math.random() * 255) + 1) + ', ' + (Math.floor(Math.random() * 255) + 1) + ')';
+    if(circulos[index].id === 'answer') {
+      let corSelecionada = circulos[index].style.backgroundColor;
+      let colorToGuess = document.getElementById('rgb-color');
+      colorToGuess.innerText = corSelecionada.replace('rgb', "");
+    }
   }
 }
