0016b445e4dc69a7f01682a49893bf63569f6860
thosijulio <thosijulio@gmail.com>
2021-03-13 19:02:30 -0300

    resolucao exercicio 5

--- 1.INTRODUCAO/5.9-MISTERY-LETTER/script.js
+++ 1.INTRODUCAO/5.9-MISTERY-LETTER/script.js
@@ -1,12 +1,21 @@
 function createLetter() {
-  let botaoGerarCarta = document.getElementById('criar-carta');
-  botaoGerarCarta.addEventListener('click', function() {
-    let mensagemDigitada = (document.getElementById('carta-texto').value).split(" ");
-    for (index = 0; index < mensagemDigitada.length; index += 1) {
-      let cartaGerada = document.getElementById('carta-gerada');
-      let spanPalavra = document.createElement('span');
-      spanPalavra.innerText = mensagemDigitada[index];
-      cartaGerada.appendChild(spanPalavra);
+  const botaoGerarCarta = document.getElementById('criar-carta');
+  botaoGerarCarta.addEventListener('click', function () {
+    const cartaGerada = document.getElementById('carta-gerada');
+    if((document.getElementById('carta-texto').value).trim().length === 0) {
+      cartaGerada.innerText = '';
+      cartaGerada.innerText = 'Por favor, digite o conteúdo da carta';
+    }
+    else {
+      for(let index = 0; index < cartaGerada.childElementCount; index += 1) {
+        cartaGerada.innerHTML = '';
+      }
+      let mensagemDigitada = (document.getElementById('carta-texto').value).split(' ');
+      for (let index = 0; index < mensagemDigitada.length; index += 1) {
+        const spanPalavra = document.createElement('span');
+        spanPalavra.innerText = mensagemDigitada[index];
+        cartaGerada.appendChild(spanPalavra);
+      }
     }
   })
 }
