7ef7092738055139e04a5665accffa3e694c694d
thosijulio <thosijulio@gmail.com>
2021-03-30 17:43:22 -0300

    resolvendo erros do eslint - Exercicio 3

--- 1.INTRODUCAO/7.4-JS-UNIT-TESTS/src/vqv.js
+++ 1.INTRODUCAO/7.4-JS-UNIT-TESTS/src/vqv.js
@@ -17,16 +17,15 @@
 */
 
 const vqv = (nome, idade) => {
-
   if (nome === undefined && idade === undefined) {
-  return undefined;
-  };
+    return undefined;
+  }
 
   const frase = `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
   return frase;
-}
+};
 
 module.exports = vqv;
