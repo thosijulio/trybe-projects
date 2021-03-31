24491a007cc105e7112b91c6a5d5d45a8e3b48bd
thosijulio <thosijulio@gmail.com>
2021-03-31 20:01:52 -0300

    correcao V6 ESLINT exercicio 9

--- 1.INTRODUCAO/7.4-JS-UNIT-TESTS/src/restaurant.js
+++ 1.INTRODUCAO/7.4-JS-UNIT-TESTS/src/restaurant.js
@@ -80,22 +80,22 @@
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
 const createMenu = (menu) => {
-  const menuRestaurant = {};
-
-  menuRestaurant.fetchMenu = () => menu;
-  menuRestaurant.fetchMenu();
-  menuRestaurant.order = (string) => menuRestaurant.consumption.push(string);
-  menuRestaurant.pay = () => {
-    let price = 0;
-    menuRestaurant.consumption.forEach((element) => {
-      if (element in menu.food) {
-        price += menu.food[element];
-      } else if (element in menu.drink) {
-        price += menu.drink[element];
-      }
-    });
-    return parseFloat(((price) * 1.10).toPrecision(4));
+  const menuRestaurant = { fetchMenu: () => menu,
+    order: (string) => menuRestaurant.consumption.push(string),
+    pay: () => {
+      let price = 0;
+      menuRestaurant.consumption.forEach((element) => {
+        if (element in menu.food) {
+          price += menu.food[element];
+        } else if (element in menu.drink) {
+          price += menu.drink[element];
+        }
+      });
+      return parseFloat(((price) * 1.10).toPrecision(4));
+    },
   };
+
+  menuRestaurant.fetchMenu();
   return menuRestaurant;
 };
 
