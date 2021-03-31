82ff8f05745d64bad0cc00b5d0b93fe01219f214
thosijulio <thosijulio@gmail.com>
2021-03-31 20:10:43 -0300

    correcao V1 exercicio 9

--- 1.INTRODUCAO/7.4-JS-UNIT-TESTS/src/restaurant.js
+++ 1.INTRODUCAO/7.4-JS-UNIT-TESTS/src/restaurant.js
@@ -80,7 +80,7 @@
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
 const createMenu = (menu) => {
-  const menuRestaurant = { fetchMenu: () => menu,
+  const menuRestaurant = { fetchMenu: menu,
     order: (string) => menuRestaurant.consumption.push(string),
     pay: () => {
       let price = 0;
