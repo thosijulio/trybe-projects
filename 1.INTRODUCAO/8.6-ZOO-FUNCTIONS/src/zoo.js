f5aeed10e9c9e47f5472e79f433f4ef3fb2985cd
thosijulio <thosijulio@gmail.com>
2021-04-13 16:07:29 -0300

    exercicio 1

---
+++ 1.INTRODUCAO/8.6-ZOO-FUNCTIONS/src/zoo.js
@@ -0,0 +1,85 @@
+/*
+eslint no-unused-vars: [
+  "error",
+  {
+    "args": "none",
+    "vars": "local",
+    "varsIgnorePattern": "data"
+  }
+]
+*/
+
+const data = require('./data');
+
+function animalsByIds(ids) {
+  let animalsId = [];
+  ids.forEach((idF) => {
+    if (ids.length > 0) {
+    animalsId.push(animals.find((curr) => curr.id === idF));
+    }});
+  return animalsId;
+}
+
+function animalsOlderThan(animal, age) {
+  // seu código aqui
+}
+
+function employeeByName(employeeName) {
+  // seu código aqui
+}
+
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+}
+
+function isManager(id) {
+  // seu código aqui
+}
+
+function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  // seu código aqui
+}
+
+function animalCount(species) {
+  // seu código aqui
+}
+
+function entryCalculator(entrants) {
+  // seu código aqui
+}
+
+function animalMap(options) {
+  // seu código aqui
+}
+
+function schedule(dayName) {
+  // seu código aqui
+}
+
+function oldestFromFirstSpecies(id) {
+  // seu código aqui
+}
+
+function increasePrices(percentage) {
+  // seu código aqui
+}
+
+function employeeCoverage(idOrName) {
+  // seu código aqui
+}
+
+module.exports = {
+  entryCalculator,
+  schedule,
+  animalCount,
+  animalMap,
+  animalsByIds,
+  employeeByName,
+  employeeCoverage,
+  addEmployee,
+  isManager,
+  animalsOlderThan,
+  oldestFromFirstSpecies,
+  increasePrices,
+  createEmployee,
+};
