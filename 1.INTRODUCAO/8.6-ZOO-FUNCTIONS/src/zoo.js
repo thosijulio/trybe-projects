4fb0fc939a4e7a2217481e8e94cc518d80498b33
thosijulio <thosijulio@gmail.com>
2021-04-15 20:57:22 -0300

    resolucao exercicio 10

--- 1.INTRODUCAO/8.6-ZOO-FUNCTIONS/src/zoo.js
+++ 1.INTRODUCAO/8.6-ZOO-FUNCTIONS/src/zoo.js
@@ -9,6 +9,9 @@ eslint no-unused-vars: [
 ]
 */
 
+// const { stat } = require('node:fs');
+// const { assert } = require('node:console');
+const assert = require('assert');
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -90,15 +93,102 @@ function entryCalculator(entrants) {
   return valueTotal;
 }
 
-function animalMap(options) {
-  // seu código aqui
-  return options;
+/* function AMON() {
+  const states = {};
+  data.animals.forEach((animal) => states[animal.location] = []);
+  
+  Object.keys(states).forEach((state) => {
+    data.animals.filter((animal) => {
+      animal.location === state ? states[state].push(animal.name) : 0;
+    });
+  });
+
+  return states;
 }
 
-function schedule(dayName) {
-  // seu código aqui
-  return dayName;
+function AMINT() {
+  const states = {};
+  data.animals.forEach((animal) => states[animal.location] = []);
+  Object.keys(states).forEach((state) => {
+    states[state] = [];
+    const animals = data.animals.filter((animal) => animal.location === state);
+    animals.forEach((aS, index) => {
+      states[state].push({});
+      states[state][index][aS.name] = aS.residents.map((animal2) => animal2.name);
+    })
+  });
+  return states;
 }
+
+function AMINTS(sex) {
+  const states = {};
+  data.animals.forEach((animal) => states[animal.location] = []);
+  Object.keys(states).forEach((state) => {
+    states[state] = [];
+    const animals = data.animals.filter((animal) => animal.location === state);
+    animals.forEach((aS, index) => {
+      states[state].push({});
+      states[state][index][aS.name] = [];
+      if (!sex) {
+        states[state][index][aS.name].push(aS.residents.map((animal2) => animal2.name));
+      } else {
+        states[state][index][aS.name].push(aS.residents.map((animal2) => animal2.sex === sex ? animal2.name : 0));
+      }
+    })
+    console.log(states.SE.penguins);
+  });
+}
+
+console.log(AMINTS());
+
+function AMINTON(list) {
+  const disorder = list;
+  Object.keys(disorder).forEach((state) => {
+    disorder[state].forEach((animal, index) => {
+      const aniN = Object.keys(animal)[0];
+      disorder[state][index][aniN].sort();
+    })
+  })
+  return disorder;
+}
+*/
+function animalMap(options) {
+  if (options.includeNames === true) {
+    if (options.sorted === true) {
+
+      return AMINTON;
+    }
+    return AMINT();
+  }
+
+
+  return AMON();
+}
+
+
+function schedule(dayName) {
+  const dH = data.hours;
+  if (dayName && dayName !== 'Monday') {
+    const day = dH[`${dayName}`];
+    const agenda = {
+      [dayName]: `Open from ${day.open}am until ${day.close}pm`,
+    }
+    return agenda;
+  }
+  if (dayName === 'Monday') return { 'Monday': 'CLOSED' };
+  const agenda = {
+    'Tuesday': 'Open from 8am until 6pm',
+    'Wednesday': 'Open from 8am until 6pm',
+    'Thursday': 'Open from 10am until 8pm',
+    'Friday': 'Open from 10am until 8pm',
+    'Saturday': 'Open from 8am until 10pm',
+    'Sunday': 'Open from 8am until 8pm',
+    'Monday': 'CLOSED'
+  }
+  return agenda;
+}
+
+console.log(schedule('Monday'))
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
