ed80b01ff0a436db7a3cd967c5677bde0b7240b9
thosijulio <thosijulio@gmail.com>
2021-05-04 01:22:34 -0300

    resolucao exercicio 3

--- 1.INTRODUCAO/10.4-JEST/test/mockApi.spec.js
+++ 1.INTRODUCAO/10.4-JEST/test/mockApi.spec.js
@@ -22,14 +22,28 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+jest.mock('../src/mockApi', () => ({
+  fetchURL: jest.fn().mockResolvedValue({
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  }),
+}));
+
 describe('verifica o usuário', () => {
-  api.fetchURL = jest.fn();
-  api.fetchURL.mockImplementation(() => {
-
-  });
-
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
+      expect.assertions(7);
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
