42103408e7271bfb65b5cc0bd1685d2f04b5d0be
thosijulio <thosijulio@gmail.com>
2021-05-10 19:44:40 -0300

    resolucao exercicio 11

--- 2.FRONT-END/11.3-MOVIE-CARDS-LIBRARY/src/components/Rating.jsx
+++ 2.FRONT-END/11.3-MOVIE-CARDS-LIBRARY/src/components/Rating.jsx
@@ -1,1 +1,13 @@
 // implement Rating component here
+import React from 'react';
+import data from '../data';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <h1>Teste</h1>
+    );
+  }
+}
+
+export default Rating;
