5c23b3af76733ed2f259a0fd35390d7e5eff6734
thosijulio <thosijulio@gmail.com>
2021-05-09 22:13:48 -0300

    resolucao exercicio 2, 3 e 15

--- 2.FRONT-END/11.3-MOVIE-CARDS-LIBRARY/src/App.js
+++ 2.FRONT-END/11.3-MOVIE-CARDS-LIBRARY/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-
-/*
-import MovieCard from './components/MovieCard';
 import MovieList from './components/MovieList';
-import Rating from './components/Rating';
-*/
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList data={ data } />
     </div>
   );
 }
