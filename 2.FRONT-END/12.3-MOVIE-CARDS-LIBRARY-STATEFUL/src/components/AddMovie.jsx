3caae0b48fcb2b6048135ba67c17da8a6555da96
thosijulio <thosijulio@gmail.com>
2021-05-15 00:19:41 -0300

    correcao V3 exercicio 8

--- 2.FRONT-END/12.3-MOVIE-CARDS-LIBRARY-STATEFUL/src/components/AddMovie.jsx
+++ 2.FRONT-END/12.3-MOVIE-CARDS-LIBRARY-STATEFUL/src/components/AddMovie.jsx
@@ -27,6 +27,7 @@ class AddMovie extends React.Component {
 
   render() {
     const { title, subtitle, imagePath, storyLine, rating, genre } = this.state;
+    const salva = subtitle + imagePath + storyLine + rating + genre;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title" data-testeid="title-input-label">
@@ -39,12 +40,8 @@ class AddMovie extends React.Component {
             value={ title }
             onChange={ this.handleFormChange }
           />
+          <p>{ salva }</p>
         </label>
-        <input value={ subtitle } onChange={ this.handleFormChange } />
-        <input value={ imagePath } onChange={ this.handleFormChange } />
-        <input value={ storyLine } onChange={ this.handleFormChange } />
-        <input value={ rating } onChange={ this.handleFormChange } />
-        <input value={ genre } onChange={ this.handleFormChange } />
       </form>
     );
   }
