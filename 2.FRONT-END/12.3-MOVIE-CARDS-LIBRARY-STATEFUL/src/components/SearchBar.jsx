63c47ecd77e8522605b2590e0fd49d14bfbd9eb0
thosijulio <thosijulio@gmail.com>
2021-05-14 22:20:41 -0300

    resolucao exercicio 3

--- 2.FRONT-END/12.3-MOVIE-CARDS-LIBRARY-STATEFUL/src/components/SearchBar.jsx
+++ 2.FRONT-END/12.3-MOVIE-CARDS-LIBRARY-STATEFUL/src/components/SearchBar.jsx
@@ -1,14 +1,31 @@
 // implement AddMovie component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <input />
+        <label htmlFor="search-input" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            id="search-bar-input"
+            name="search-bar-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+            type="text"
+          />
+        </label>
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
