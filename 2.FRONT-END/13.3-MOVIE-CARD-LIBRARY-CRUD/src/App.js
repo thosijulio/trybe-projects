ee42d9a3aa5de746f27b451a384f393a878f4ecf
thosijulio <thosijulio@gmail.com>
2021-05-29 15:15:26 -0300

    correcao V1 exercicio 1

--- 2.FRONT-END/13.3-MOVIE-CARD-LIBRARY-CRUD/src/App.js
+++ 2.FRONT-END/13.3-MOVIE-CARD-LIBRARY-CRUD/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import EditMovie from './pages/EditMovie';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import MovieList from './pages/MovieList';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
         <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route path="/movies/:id" component={ MovieDetails } />
         <Route exact path="/" component={ MovieList } />
+        <Route path="/404" component={ NotFound } />
+        <Redirect to="/404" />
       </Switch>
     </BrowserRouter>
   );
