e6f662939e69e33a14936daf62c7580989e607ca
thosijulio <thosijulio@gmail.com>
2021-05-30 11:17:54 -0300

    correcao V1 exercicio 4

--- 2.FRONT-END/13.3-MOVIE-CARD-LIBRARY-CRUD/src/pages/MovieDetails.js
+++ 2.FRONT-END/13.3-MOVIE-CARD-LIBRARY-CRUD/src/pages/MovieDetails.js
@@ -43,7 +43,7 @@ class MovieDetails extends Component {
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
-        <Link to={ `movies/${id}/edit` }>EDITAR</Link>
+        <Link to={ `./${id}/edit` }>EDITAR</Link>
         <Link to="/" style={ ({ margin: '15px' }) }>VOLTAR</Link>
       </div>
     );
