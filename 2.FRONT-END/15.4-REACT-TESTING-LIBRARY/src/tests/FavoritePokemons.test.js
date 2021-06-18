1e5f8b84c094151f1729980c9bce5891dd83306f
thosijulio <thosijulio@gmail.com>
2021-06-18 01:39:03 -0300

    correcao v1 exercicio 3

--- 2.FRONT-END/15.4-REACT-TESTING-LIBRARY/src/tests/FavoritePokemons.test.js
+++ 2.FRONT-END/15.4-REACT-TESTING-LIBRARY/src/tests/FavoritePokemons.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
-import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import { FavoritePokemons } from '../components';
 
