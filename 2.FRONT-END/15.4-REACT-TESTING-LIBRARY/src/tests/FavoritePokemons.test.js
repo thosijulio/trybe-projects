4a7cd785f954a237de3849367e7551d326c83b73
thosijulio <thosijulio@gmail.com>
2021-06-18 01:38:15 -0300

    resolucao exercicio 3

--- 2.FRONT-END/15.4-REACT-TESTING-LIBRARY/src/tests/FavoritePokemons.test.js
+++ 2.FRONT-END/15.4-REACT-TESTING-LIBRARY/src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Teste do component FavoritePokemons', () => {
+  test('exibido na tela uma mensagem caso nao exista pokes favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFavMessage = screen.getByText('No favorite pokemon found');
+
+    expect(notFavMessage).toBeDefined();
+  });
+});
