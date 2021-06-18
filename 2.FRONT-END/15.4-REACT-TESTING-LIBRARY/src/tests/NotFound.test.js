3ab606f409464694f7d9a2086ff3963781ce1591
thosijulio <thosijulio@gmail.com>
2021-06-18 01:59:09 -0300

    resolucao exercicio 4

--- 2.FRONT-END/15.4-REACT-TESTING-LIBRARY/src/tests/NotFound.test.js
+++ 2.FRONT-END/15.4-REACT-TESTING-LIBRARY/src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testa o component NotFound', () => {
+  test('a pagina contem um h2 com um texto especifico', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundText = screen.getByRole(
+      'heading',
+      { level: 2, name: 'Page requested not found Crying emoji' },
+    );
+    const notFoundImage = screen.getAllByRole('img');
+
+    expect(notFoundText).toBeDefined();
+    expect(notFoundImage[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
