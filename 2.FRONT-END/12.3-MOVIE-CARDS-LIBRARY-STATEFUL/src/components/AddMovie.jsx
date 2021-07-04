63f4d249eb0031a25ac5239d16d6dea12348c181
thosijulio <thosijulio@gmail.com>
2021-07-04 09:30:38 -0300

    resolucao exercicio 6

--- 2.FRONT-END/12.3-MOVIE-CARDS-LIBRARY-STATEFUL/src/components/AddMovie.jsx
+++ 2.FRONT-END/12.3-MOVIE-CARDS-LIBRARY-STATEFUL/src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <p>{ subtitle + title + imagePath + storyline + rating + genre }</p>
+    );
+  }
+}
+
+export default AddMovie;
