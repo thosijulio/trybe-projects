1fbb58f2d26d848063498aef77c7a6181b48d8a2
thosijulio <thosijulio@gmail.com>
2021-07-04 01:35:14 -0300

    resolucao exercicio 3

--- 2.FRONT-END/12.3-MOVIE-CARDS-LIBRARY-STATEFUL/src/components/SearchBar.jsx
+++ 2.FRONT-END/12.3-MOVIE-CARDS-LIBRARY-STATEFUL/src/components/SearchBar.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
     return (
-      <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-inp" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            id="text-inp"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        <p>{ bookmarkedOnly + selectedGenre }</p>
+        <p>{ onBookmarkedChange + onSelectedGenreChange }</p>
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
