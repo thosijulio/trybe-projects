74460a1d2ad0ba2767d5c2a1f438d2a96381912f
thosijulio <thosijulio@gmail.com>
2021-07-04 09:57:20 -0300

    resolucao exercicio 8

--- 2.FRONT-END/12.3-MOVIE-CARDS-LIBRARY-STATEFUL/src/components/AddMovie.jsx
+++ 2.FRONT-END/12.3-MOVIE-CARDS-LIBRARY-STATEFUL/src/components/AddMovie.jsx
@@ -11,12 +11,33 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleFormChange = this.handleFormChange.bind(this);
+  }
+
+  handleFormChange(event) {
+    const name = event.target.id;
+    const { target: { value } } = event;
+
+    this.setState((oldState) => ({
+      ...oldState,
+      [name]: value,
+    }));
   }
 
   render() {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
+        <label htmlFor="title-inp" data-testid="title-input-label">
+          Título
+          <input
+            id="title"
+            type="text"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleFormChange }
+          />
+        </label>
         <p>{ subtitle + title + imagePath + storyline + rating + genre }</p>
       </form>
     );
