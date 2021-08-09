3fa97c918c7cfb19151153a3d48525c232b7d7ec
thosijulio <thosijulio@gmail.com>
2021-08-09 10:58:51 -0300

    resoluca oexercicio 3

--- 2.FRONT-END/18.4-STARWARS-DATATABLE/src/components/PlanetsFilter.js
+++ 2.FRONT-END/18.4-STARWARS-DATATABLE/src/components/PlanetsFilter.js
@@ -1,15 +1,74 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import MainContext from '../context/MainContext';
 
-function PlanetsFilter() {
-  const { data, setFilteredData } = useContext(MainContext);
+const listFilter = [
+  'population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water'];
 
-  function handleFormChange(value) {
+function PlanetsFilter() {
+  const {
+    data,
+    filters,
+    setFilters,
+    filteredData,
+    setFilteredData,
+  } = useContext(MainContext);
+
+  const [listFilterChoise, setListFilterChoise] = useState('population');
+  const [comparisonFilter, setComparisonFilter] = useState('maior que');
+  const [valueFilter, setValueFilter] = useState(0);
+
+  function handleNameChange(value) {
     const newData = data.filter((planet) => {
       if (planet.name.includes(value)) return true;
       return false;
     });
     setFilteredData(newData);
+    setFilters({ ...filters, filterByName: { name: value } });
+  }
+
+  async function handleNumericChange() {
+    listFilter.splice(listFilter.indexOf(listFilterChoise) || 0, 1);
+    await setFilters({
+      ...filters,
+      filterByNumericValues: [
+        ...filters.filterByNumericValues,
+        {
+          column: listFilterChoise,
+          comparison: comparisonFilter,
+          value: valueFilter,
+        },
+      ],
+    });
+
+    switch (comparisonFilter) {
+    case 'maior que':
+      setFilteredData(filteredData.filter((planet) => {
+        if (parseInt(planet[listFilterChoise], 10) > valueFilter) return true;
+        return false;
+      }));
+      break;
+
+    case 'menor que':
+      setFilteredData(filteredData.filter((planet) => {
+        if (parseInt(planet[listFilterChoise], 10) < valueFilter) return true;
+        return false;
+      }));
+      break;
+
+    case 'igual a':
+      setFilteredData(filteredData.filter((planet) => {
+        if (parseInt(planet[listFilterChoise], 10) === valueFilter) return true;
+        return false;
+      }));
+      break;
+
+    default:
+      setFilteredData(data);
+    }
   }
 
   return (
@@ -22,9 +81,49 @@ function PlanetsFilter() {
             id="name-filter"
             data-testid="name-filter"
             placeholder="Tatooine"
-            onChange={ ({ target: { value } }) => handleFormChange(value) }
+            onChange={ ({ target: { value } }) => handleNameChange(value) }
           />
         </label>
+        <p>Valores numéricos:</p>
+        <label htmlFor="column-filter">
+          Filtro:
+          <select
+            data-testid="column-filter"
+            id="column-filter"
+            onChange={ ({ target: { value } }) => setListFilterChoise(value) }
+            value={ listFilterChoise }
+          >
+            { listFilter.map((filter) => <option key={ filter }>{filter}</option>)}
+          </select>
+        </label>
+        <label htmlFor="comparison-filter">
+          Faixa de valor:
+          <select
+            data-testid="comparison-filter"
+            id="comparison-filter"
+            value={ comparisonFilter }
+            onChange={ ({ target: { value } }) => setComparisonFilter(value) }
+          >
+            <option>maior que</option>
+            <option>menor que</option>
+            <option>igual a</option>
+          </select>
+        </label>
+        <label htmlFor="column-filter">
+          Valor:
+          <input
+            type="number"
+            onChange={ ({ target: { value } }) => setValueFilter(value) }
+            value={ valueFilter }
+          />
+        </label>
+        <button
+          type="button"
+          onClick={ handleNumericChange }
+          data-testid="value-filter"
+        >
+          Filtrar por valor
+        </button>
       </fieldset>
     </form>
   );
