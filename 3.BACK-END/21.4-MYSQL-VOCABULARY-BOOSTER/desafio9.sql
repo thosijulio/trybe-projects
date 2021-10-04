157e46ff624ecd373f89e5f796958042923c95ea
thosijulio <thosijulio@gmail.com>
2021-10-04 16:32:04 -0300

    resolucao requisito 9

---
+++ 3.BACK-END/21.4-MYSQL-VOCABULARY-BOOSTER/desafio9.sql
@@ -0,0 +1,9 @@
+SELECT 
+    CONCAT(E.FirstName, ' ', E.LastName) AS `Nome completo`,
+    COUNT(*) AS `Total de pedidos`
+FROM
+    w3schools.employees AS E
+        INNER JOIN
+    w3schools.orders AS O ON O.EmployeeID = E.EmployeeID
+GROUP BY E.EmployeeID
+ORDER BY `Total de pedidos`;
