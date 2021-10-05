da329d374797f6dbd4924d55944cd55edcc8f140
thosijulio <thosijulio@gmail.com>
2021-10-05 08:59:50 -0300

    correcao requisito 11

--- 3.BACK-END/21.4-MYSQL-VOCABULARY-BOOSTER/desafio11.sql
+++ 3.BACK-END/21.4-MYSQL-VOCABULARY-BOOSTER/desafio11.sql
@@ -1,12 +1,13 @@
 SELECT 
     C1.ContactName AS Nome,
     C1.Country AS País,
-    (SELECT 
-            COUNT(*)
-        FROM
-            w3schools.customers AS C2
-        WHERE
-            C1.Country = C2.Country) AS `Número de compatriotas`
+    COUNT(C2.Country) AS `Número de compatriotas`
 FROM
-    w3schools.customers AS C1
+    w3schools.customers AS C1,
+    w3schools.customers AS C2
+WHERE
+    C1.Country = C2.Country
+        AND C1.ContactName <> C2.ContactName
+GROUP BY C1.ContactName , C1.Country
+HAVING `Número de compatriotas` > 0
 ORDER BY C1.ContactName;
