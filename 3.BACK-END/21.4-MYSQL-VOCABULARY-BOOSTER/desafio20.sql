c6d6fcacd16628d035f2f3e3d098373f98e90e18
thosijulio <thosijulio@gmail.com>
2021-10-05 15:41:11 -0300

    resolucao requisito 20

--- 3.BACK-END/21.4-MYSQL-VOCABULARY-BOOSTER/desafio20.sql
+++ 3.BACK-END/21.4-MYSQL-VOCABULARY-BOOSTER/desafio20.sql
@@ -12,10 +12,11 @@ FROM
         INNER JOIN
     employees AS E ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
         INNER JOIN
-    departments AS D ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+    departments AS D ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
         INNER JOIN
-    jobs AS J ON J.JOB_ID = E.JOB_ID
-WHERE E.EMAIL = emailRequired;
+    jobs AS J ON J.JOB_ID = JH.JOB_ID
+WHERE E.EMAIL = emailRequired
+ORDER BY `Departamento`, `Cargo`;
 END $$
 
 DELIMITER ;
