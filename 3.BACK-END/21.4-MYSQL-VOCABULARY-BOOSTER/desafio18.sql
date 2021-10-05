fb1342edacfeca9d9ae59084697f342ae2772aca
thosijulio <thosijulio@gmail.com>
2021-10-05 12:08:48 -0300

    resolucao requisito 18

--- 3.BACK-END/21.4-MYSQL-VOCABULARY-BOOSTER/desafio18.sql
+++ 3.BACK-END/21.4-MYSQL-VOCABULARY-BOOSTER/desafio18.sql
@@ -1,1 +1,11 @@
-teste
+SELECT 
+    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+    JH.START_DATE AS `Data de início`,
+    JH.END_DATE AS `Data de rescisão`,
+    ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE) / 365.25,
+            2) AS `Anos trabalhados`
+FROM
+    hr.employees AS E
+        INNER JOIN
+    hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+ORDER BY `Nome completo` , `Anos trabalhados`;
