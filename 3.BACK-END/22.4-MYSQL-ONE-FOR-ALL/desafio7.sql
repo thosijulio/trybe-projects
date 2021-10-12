7942e182489bac925c0d54c583ad2369a3f19ba6
thosijulio <thosijulio@gmail.com>
2021-10-12 20:10:02 -0300

    resolucao requisito 7

---
+++ 3.BACK-END/22.4-MYSQL-ONE-FOR-ALL/desafio7.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ART.Nome AS artista,
+    ALB.Nome AS album,
+    COUNT(*) AS seguidores
+FROM
+    Artista AS ART
+        INNER JOIN
+    Album AS ALB ON ART.Id = ALB.Id_Artista
+        INNER JOIN
+    Usuario_Artista AS USU_ART ON USU_ART.Id_Artista = ART.Id
+GROUP BY artista, album
+ORDER BY seguidores DESC , artista , album;
