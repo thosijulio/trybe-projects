65ac1cc9cb5fb509a38cd285bc39a977cecbdb13
thosijulio <thosijulio@gmail.com>
2021-10-12 19:51:01 -0300

    resolucao requisito 5

--- 3.BACK-END/22.4-MYSQL-ONE-FOR-ALL/desafio4.sql
+++ 3.BACK-END/22.4-MYSQL-ONE-FOR-ALL/desafio4.sql
@@ -1,10 +1,6 @@
 CREATE VIEW top_3_artistas AS
-	SELECT 
-		A.Nome AS artista, COUNT(*) AS seguidores
-	FROM
-		Artista AS A
-			INNER JOIN
-		Usuario_Artista AS UA ON UA.Id_Artista = A.Id
-	GROUP BY artista
-	ORDER BY seguidores DESC
-	LIMIT 3;
+    SELECT A.Nome AS artista, COUNT(*) AS seguidores
+    FROM Artista AS A INNER JOIN Usuario_Artista AS UA ON UA.Id_Artista = A.Id
+    GROUP BY artista
+    ORDER BY seguidores DESC, artista
+    LIMIT 3;
