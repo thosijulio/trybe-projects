5c0be8c81e13f2415b14e3d54ac7e9317f1150f0
thosijulio <thosijulio@gmail.com>
2021-10-20 13:52:22 -0300

    resolucao requisito 19

--- 3.BACK-END/24.4-MONGODB-COMMERCE/scripts/desafio17.js
+++ 3.BACK-END/24.4-MONGODB-COMMERCE/scripts/desafio17.js
@@ -1,8 +1,9 @@
-db.produtos.createIndex({ descricao: "text" });
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+  );
 
 db.produtos.count({
   $text: {
     $search: "frango hamburguer",
-    $language: "pt",
-  } },
-);
+  } });
