ea506153ba73db0e69063fa535d2fe71ffb81d70
thosijulio <thosijulio@gmail.com>
2021-10-20 13:11:26 -0300

    resolucao requisito 11

--- 3.BACK-END/24.4-MONGODB-COMMERCE/scripts/desafio10.js
+++ 3.BACK-END/24.4-MONGODB-COMMERCE/scripts/desafio10.js
@@ -1,7 +1,15 @@
 db.produtos.updateMany(
   {},
   { $set: {
-    vendasPorDia: [NumberInt(0), NumberInt(0), NumberInt(0), NumberInt(0), NumberInt(0), NumberInt(0), NumberInt(0),],
+    vendasPorDia: [
+      NumberInt(0),
+      NumberInt(0),
+      NumberInt(0),
+      NumberInt(0),
+      NumberInt(0),
+      NumberInt(0),
+      NumberInt(0),
+    ],
   } },
 );
 
@@ -11,6 +19,25 @@ db.produtos.updateOne(
     vendasPorDia: {
       $each: [60],
       $position: 3,
-      $slice: 7
+      $slice: 7,
     } } },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["bovino", "pão"] } },
+  { $push: {
+    vendasPorDia: {
+      $each: [120],
+      $position: 6,
+      $slice: 7,
+    } } },
+);
+
+db.produtos.find(
+  {},
+  {
+    nome: true,
+    vendasPorDia: true,
+    _id: false,
+  },
 );
