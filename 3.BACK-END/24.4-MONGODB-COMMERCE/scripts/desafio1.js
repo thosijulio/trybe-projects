0a3c9bbc621378b1944af913b380b54f443458fb
thosijulio <thosijulio@gmail.com>
2021-10-20 10:37:46 -0300

    resolucao requisito 2

--- 3.BACK-END/24.4-MONGODB-COMMERCE/scripts/desafio1.js
+++ 3.BACK-END/24.4-MONGODB-COMMERCE/scripts/desafio1.js
@@ -1,1 +1,6 @@
-db.produtos.updateMany({}, { criadoPor: "Ronald McDonald"});
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
+
+db.produtos.find(
+  {},
+  { nome: true, criadoPor: true, _id: false },
+);
