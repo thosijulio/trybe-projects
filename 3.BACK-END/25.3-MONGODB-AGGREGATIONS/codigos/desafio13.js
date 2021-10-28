52bd0217bab8137311252bf19c18e681041679f7
thosijulio <thosijulio@gmail.com>
2021-10-28 09:56:44 -0300

    resolucao requisito 13

---
+++ 3.BACK-END/25.3-MONGODB-AGGREGATIONS/codigos/desafio13.js
@@ -0,0 +1,53 @@
+db.trips.aggregate([
+  {
+    $match: {
+      $expr: {
+        $eq: [
+          {
+            $dateToString: {
+              format: "%Y-%m-%d",
+              date: "$startTime",
+            },
+          },
+          "2016-03-10",
+        ],
+      },
+    },
+  },
+  {
+    $addFields: {
+      duracao: {
+        $multiply: [
+          {
+            $divide: [
+              {
+                $subtract: [
+                  "$stopTime",
+                  "$startTime"
+                ],
+              },
+              3600000,
+            ],
+          },
+          60,
+        ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: {
+        $avg: "$duracao",
+      },
+    },
+  },
+  {
+    $project: {
+      _id: false,
+      duracaoMediaEmMinutos: {
+        $ceil: "$duracaoMediaEmMinutos",
+      },
+    },
+  },
+]);
