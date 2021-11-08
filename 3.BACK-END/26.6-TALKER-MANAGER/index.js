0c5b4edbadbb80a0185b04fd6d3cb7f91df91d63
thosijulio <thosijulio@gmail.com>
2021-11-08 13:15:20 -0300

    resolucao requsiito 2

--- 3.BACK-END/26.6-TALKER-MANAGER/index.js
+++ 3.BACK-END/26.6-TALKER-MANAGER/index.js
@@ -8,6 +8,14 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
+  const talker = talkers.find((val) => val.id === parseInt(id, 10));
+  if (talker) return res.status(200).json(talker);
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -18,15 +26,7 @@ app.listen(PORT, () => {
 });
 
 app.get('/talker', async (req, res) => {
-  const talkers = await JSON.parse(fs.readFile('./talker.json', 'utf-8'));
+  const talkers = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
   if (talkers.length) return res.status(200).json(talkers);
   return res.status(200).json([]);
-});
-
-app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
-  const talkers = await JSON.parse(fs.readFile('./talker.json', 'utf-8'));
-  const talker = talkers.find((val) => val.id === parseInt(id, 10));
-  if (talker) return res.status(200).json(talker);
-  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
