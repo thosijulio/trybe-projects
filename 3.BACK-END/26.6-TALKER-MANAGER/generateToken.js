c09e975643c139291ccde774f4c279a67a414bba
thosijulio <thosijulio@gmail.com>
2021-11-08 15:45:45 -0300

    criando script para criacao do token

---
+++ 3.BACK-END/26.6-TALKER-MANAGER/generateToken.js
@@ -0,0 +1,4 @@
+module.exports = (
+  Math.random().toString(36).substring(2)
+  + Math.random().toString(36).substring(2)
+).substring(0, 16);
--- 3.BACK-END/26.6-TALKER-MANAGER/index.js
+++ 3.BACK-END/26.6-TALKER-MANAGER/index.js
@@ -8,14 +8,6 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
-  const talkers = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
-  const talker = talkers.find((val) => val.id === parseInt(id, 10));
-  if (talker) return res.status(200).json(talker);
-  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-});
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -23,6 +15,14 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
+  const talker = talkers.find((val) => val.id === parseInt(id, 10));
+  if (talker) return res.status(200).json(talker);
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 app.get('/talker', async (req, res) => {
